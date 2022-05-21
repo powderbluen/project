95ceefa8b5a83a0c473c4fe0e157ddb464a415fa
powderblue <zxp1937516045@163.com>
2022-05-21 17:02:20 +0800

    更新了主界面和用户功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,22 @@
-$.ajaxPrefilter((options) => {
-    options.url = `http://www.liulongbin.top:3007` + options.url;
-    // alert(options.url)
+$.ajaxPrefilter((option) => {
+    option.url = `http://www.liulongbin.top:3007` + option.url;
+    //在请求之前给有权限的接口注入 token
+    if (option.url.includes("/my/")) {
+        option.headers = {
+            Authorization: localStorage.getItem('token'),
+        };
+    }
+
+    //无论成功还是失败 最终都会调用complete回调函数
+    option.complete = (res) => {
+        // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            console.log(res.data);
+            //  强制清空 token
+            localStorage.removeItem("token");
+            // 强制跳转到登录页面
+            location.href = "/login2.html"
+        }
+    };
+
 })
---
+++ assets/js/index2.js
@@ -0,0 +1,53 @@
+$(function () {
+    //调用getUserInfo函数获取用户基本信息
+    getUserInfo();
+
+    const layer = layui.layer;
+    $('#btnLogout').on('click', () => {
+        layer.confirm('确定退出吗？', { icon: 3, title: '' }, function () {
+            //清空本地存储里面的登录唯一标识符token
+            localStorage.removeItem('token');
+            //重新跳转到登录页面
+            location.href = '/login2.html';
+        })
+    })
+
+})
+
+const layer = layui.layer;
+//获取用户基本信息
+function getUserInfo() {
+    $.ajax({
+        type: "GET",
+        url: '/my/userinfo',
+        headers: {
+            Authorization: localStorage.getItem('token'),
+        },
+        success: (res) => {
+            if (res.status !== 0) return layer.msg('数据请求失败');
+            layer.msg('获取用户信息成功');
+            console.log(res);
+            //调用renderAvatar函数渲染用户头像
+            renderAvatar(res.data);
+        },
+    })
+}
+
+//渲染用户头像
+const renderAvatar = (user) => {
+    //获取用户名
+    let name = user.nickname || user.username;
+    //设置欢迎文本
+    $('#welcome').html(`欢迎 ${name}`);
+    //按需渲染用户头像
+    if (user.user_pic !== null) {
+        //渲染图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide();
+    } else {
+        //渲染文本头像
+        $('.layui-nav-img').hide();
+        let firstName = name[0].toUpperCase();
+        $('.text-avatar').html(firstName);
+    }
+}
